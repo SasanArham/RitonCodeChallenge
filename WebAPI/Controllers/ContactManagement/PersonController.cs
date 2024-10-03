2290b1acec8a72037b27694437ca42f9f6a6014d
Sasan <l.s.arham@gmail.com>
2024-10-03 20:35:49 +0330

    [Add] Implement Person Post request

---
+++ WebAPI/Controllers/ContactManagement/PersonController.cs
@@ -0,0 +1,15 @@
+﻿using Application.Modules.ContactManagement.Commands;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers.ContactManagement
+{
+    public class PersonController : BaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(ImportPeopleFromExcelCommand usecase)
+        {
+            await Mediator.Send(usecase);
+            return Ok();
+        }
+    }
+}
